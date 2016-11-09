4fb44f84731091f190e6cfe86e7a884724e243b8
jorge97710 <jazmitia_10@hotmail.com>
2016-11-09 11:02:28 -0600

    Se agregan clases iniciales

---
+++ hoja11/src/Main.java
@@ -0,0 +1,18 @@
+
+/**
+ * @author Carlos Calderon , Marisol Barillas , Jorge Azmitia
+ * @version 4.1
+ * Clase Main que se encarga de ejecutar el programa.
+ *
+ */
+public class Main {
+	/**
+	 * Metodo main para correr el programa
+	 * @param args .
+	 */
+	public static void main (String [] args){
+		//ClaseFrame miVentana = new ClaseFrame();
+		//miVentana.setVisible(true);           
+
+	}
+}

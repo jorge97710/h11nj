b6fd09b030272e5717e516e02840ffca1ebb32f4
Carlos <carlos.calderon097@gmail.com>
2016-11-11 00:17:14 -0600

    Version 2.0 Se agrega GUI.

---
+++ hoja11/src/Main.java
@@ -0,0 +1,22 @@
+
+/**
+ * @author Carlos Calderon , Marisol Barillas , Jorge Azmitia
+ * @version 2.0
+ * Clase Main que se encarga de ejecutar el programa.
+ *
+ */
+public class Main {
+	/**
+	 * Metodo main para correr el programa
+	 * @param args .
+	 */
+	public static void main (String [] args){
+		//ClaseFrame miVentana = new ClaseFrame();
+		//miVentana.setVisible(true);           
+		//Manejador mn=new Manejador();
+		//mn.leerContenido();
+		//mn.hacergrafo();
+		GUI miVentana = new GUI();
+		miVentana.setVisible(true);     
+	}
+}

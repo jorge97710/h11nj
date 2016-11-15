2045fe9d9532d6e0c4935b918bb8a55427b37ab4
Carlosc23 <carlos.calderon097@gmail.com>
2016-11-14 23:51:59 -0600

    Add files via upload

---
+++ h11nj/hoja11/src/Main.java
@@ -0,0 +1,26 @@
+
+/**
+ * @author Carlos Calderon , Marisol Barillas , Jorge Azmitia
+ * @version 2.2
+ * Clase Main que se encarga de ejecutar el programa.
+ *
+ */
+public class Main {
+	/** 
+	 * Metodo main para correr el programa
+	 * @param args .
+	 */
+	public static void main (String [] args){
+		
+		/*int v=4,e=5;
+		Vertex v1 = new Vertex("Carlos","c",0);
+		Vertex v2 = new Vertex("anrea","a",1);          
+    	FloydWarshall<String> fw = new FloydWarshall<String>(4);
+    	fw.addEdge(v1, v2, 5); 
+    	fw.addEdge(v2, v1, 4);*/
+                  
+    	GUI miVentana = new GUI();
+		miVentana.setVisible(true); 
+	}
+}
+      
---
+++ h11nj/hoja11/src/Manejador.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.swing.JOptionPane;
+
+
+
+/**
+ * @author Carlos Calderon , Marisol Barillas , Jorge Azmitia
+ * @version 2.2
+ */
+public class Manejador {
+	
+	private FloydWarshall fw;
+	private Vertex<String>[] v ;
+	private Vertex <String> aux1,aux2;
+	private Set<String> ciudades =new TreeSet<String>();;
+	private ArrayList<String> ciudadesList = new ArrayList<String>();
+	 String [] hacergrafo(ArrayList<String> array, int conta){
+		fw = new FloydWarshall(8);
+		int co=0;
+		for(int i = 0; i < array.size(); i++) {
+			String temp = array.get(i);
+			String[] partes = temp.split(" ");
+			// Anadir ciudades a cmbBox
+			ciudades.add(partes[0]);
+			ciudades.add(partes[1]);
+			co++;
+		}
+		v= new Vertex[ciudades.size()];
+		co=0;
+		for(String i: ciudades ){
+		v[co]= new Vertex<String>(i,i,co);	
+		co++;
+		}
+		for(int i = 0; i < array.size(); i++) {
+			String temp = array.get(i);
+			String[] partes = temp.split(" ");
+			for(Vertex j: v ){
+					if (j.getName().equals(partes[0])){
+						aux1=j;
+					}
+				}
+			for(Vertex j: v ){
+				if (j.getName().equals(partes[1])){
+					aux2=j;
+				}
+			}
+			fw.addEdge(aux1, aux2, Double.parseDouble(partes[2]));
+		}
+		ciudadesList.addAll(ciudades);
+		String [] c =  ciudadesList.toArray(new String[ciudadesList.size()]);
+		return c;
+	}
+	 String corta(int origen, int destino,String o , String d){
+		 fw.floydWarshall();
+		 double a= fw.getEdge(origen,destino);
+		 if(a==Double.POSITIVE_INFINITY){
+			 return "No hay camino entre ellos";
+		 }
+		 fw.mostrarIntermedias(origen, destino);
+		 String cen = fw.centroGrafo();
+		 return "El peso de: "+o+" a: "+ d+ "es: "+a +" y la ruta es: "+v[origen].getName()+ fw.inter+"--> "+ v[ destino].getName()+ " y el centro "
+		 		+ "es: "+cen;
+	 }
+	 void modificarGrafo(int o, int d, double w ){
+		 fw.setEdge(o, d, w);
+	 }
+	
+}
